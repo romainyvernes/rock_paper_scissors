c800b41ac982339af1651e7413b3eb7b1cd34a79
Romain <romainyvernes@gmail.com>
2021-03-03 18:02:09 -0800

    Completed project

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+function displayGame() {
+    scoreboard.style.display = 'flex';
+    selectionContainer.style.display = 'flex';
+    this.style.display = 'none';
+    title.style.marginTop = '0.67em';
+    instructions.textContent = 'The first one to reach 5 points wins the game.';
+}
+
 function computerPlay() {
     let options = ['Rock', 'Paper', 'Scissors'];
     let randomPosition = Math.floor(Math.random() * 3);
@@ -5,8 +13,10 @@ function computerPlay() {
 }
 
 function playRound() {
-    let playerSelection = this.textContent;
+    let playerSelection = this.id;
     let computerSelection = computerPlay();
+
+    if (playerScore < 5 && computerScore < 5) displayHand(playerSelection, computerSelection);
     
     let duel1 = ['Rock', 'Paper'];
     let duel2 = ['Rock', 'Scissors'];
@@ -54,6 +64,7 @@ function updateScoreboard() {
             computer.textContent = computerScore;
             result.textContent = 'Game Over! Try again!';
         }
+        resetButton.style.display = 'block';
     } else {
         player.textContent = playerScore;
         computer.textContent = computerScore;
@@ -61,23 +72,49 @@ function updateScoreboard() {
     }
 }
 
+function displayHand(playerSelection, computerPlay) {
+    playerHand.textContent = playerSelection;
+    computerHand.textContent = computerPlay;
+    playerHandContainer.style.visibility = 'visible';
+    computerHandContainer.style.visibility = 'visible';
+}
+
 function reset() {
     playerScore = 0;
     computerScore = 0;
     matchResult = '';
     updateScoreboard();
+    playerHand.textContent = '';
+    computerHand.textContent = '';
+    resetButton.style.display = 'none';
+    playerHandContainer.style.visibility = 'hidden';
+    computerHandContainer.style.visibility = 'hidden';
 }
 
 let playerScore = 0;
 let computerScore = 0;
 let matchResult = '';
 
+const title = document.querySelector('h1');
+const instructions = document.querySelector('#instructions');
+
 const player = document.querySelector('#player .score');
 const result = document.querySelector('#result');
 const computer = document.querySelector('#computer .score');
 
+const playerHandContainer = document.querySelector('#player-selection');
+const computerHandContainer = document.querySelector('#computer-selection')
+const playerHand = document.querySelector('#player-selection .selection');
+const computerHand = document.querySelector('#computer-selection .selection');
+
+const scoreboard = document.querySelector('#scoreboard');
+const selectionContainer = document.querySelector('#selection-container');
+
 const selectionButtons = document.querySelectorAll('#selections button');
 selectionButtons.forEach(selectionButton => selectionButton.addEventListener('click', playRound));
+
+const startButton = document.querySelector('#start');
+startButton.addEventListener('click', displayGame);
 
 const resetButton = document.querySelector('#reset');
 resetButton.addEventListener('click', reset);
